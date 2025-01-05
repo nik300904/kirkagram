9de4fd15ab624448c87be71b5f644c8301938be7
nik300904 <nikitak3009@gmail.com>
2025-01-05 15:38:39 +0300

    separate user table to new table

---
+++ migrations/2_update_follow.up.sql
@@ -0,0 +1,16 @@
+ALTER TABLE "user"
+    DROP COLUMN IF EXISTS followers,
+    DROP COLUMN IF EXISTS following;
+
+CREATE TABLE IF NOT EXISTS "follow" (
+    id SERIAL PRIMARY KEY,
+    follower_id INTEGER NOT NULL,
+    following_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (follower_id) REFERENCES "user"(id) ON DELETE CASCADE,
+    FOREIGN KEY (following_id) REFERENCES "user"(id) ON DELETE CASCADE,
+    UNIQUE (follower_id, following_id)
+);
+
+CREATE INDEX IF NOT EXISTS follow_follower_idx ON follow(follower_id);
+CREATE INDEX IF NOT EXISTS follow_following_idx ON follow(following_id);
